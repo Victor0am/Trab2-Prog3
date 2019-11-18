7d3d4e070313c6f222491594fda0ac6fa905f194
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2019-11-18 14:43:42 -0300

    carregar Docentes no sistema funcionando :pray:

--- src/trabalho/Main.java
+++ src/trabalho/Main.java
@@ -8,79 +8,57 @@ public class Main {
     // public static ArrayList<Docente> docentesCadastrados = new ArrayList<Docente>();
     // public static ArrayList<Regra> regras = new ArrayList<Regra>();
     public static void main(String[] args) {
-        String[] flags = new String[args.length - 1];
-        /* for (int i = 1; i < args.length; i++){
-            if (args[i] == "-d"){
-                //
+        // FileReader arquivo;
+        // String[] flags = new String[args.length - 1];
+        for (int i = 1; i < args.length; i++){
+            switch (args[i]){
+            case (args[i] == "-d"){
+                FileReader arq1 = new FileReader("docentes.csv");
                 i++;
                 continue;
             }
-            if (args[i] == "-v"){
-                //
+            case (args[i] == "-v"){
+                FileReader arq2 = new FileReader("veiculos.csv");
                 i++;
                 continue;
             }
-            if (args[i] == "-p"){
-                //
+            case (args[i] == "-p"){
+                FileReader arq3 = new FileReader("publicacoes.csv");
                 i++;
+                continue;
             }
-            if (args[i] == "-q"){
-                //
+            case (args[i] == "-q"){
+                FileReader arq4 = new FileReader("qualis.csv");
                 i++;
+                continue;
             }
-            if (args[i] == "-r"){
-                //
+            case (args[i] == "-r"){
+                FileReader arq5 = new FileReader("regras.csv");
                 i++;
+                continue;
             }
-            if (args[i] == "-a"){
-                //
+            case (args[i] == "-a"){
+                FileReader arq6 = new FileReader("docentes.csv");
                 i++;
+                continue;
             }
-        } */
+            }
+        }
         Scanner leitor = new Scanner(System.in);
         Sistema ppgi = new Sistema();
         try  {
-            FileReader arquivo = new FileReader("docentes.csv");
-            BufferedReader leituraDeArquivo = new BufferedReader(arquivo);
-            String linha = leituraDeArquivo.readLine();
-            linha = leituraDeArquivo.readLine();
-            while(linha !=null){
-                String[] campos = linha.split(";");
-                long codigo = Long.parseLong(campos[0]);
-                String nome = campos[1];
-                Date nascimento = new SimpleDateFormat("dd/MM/yyyy").parse(campos[2]);
-                Date ingresso = new SimpleDateFormat("dd/MM/yyyy").parse(campos[3]);
-                boolean coordenador = false;
-                // System.out.println(linha);
-                if (campos.length == 5) {
-                    if (campos[4] == "X") {
-                        coordenador = true;
-                    }
-                }
-                Docente docente = new Docente(codigo, nome, nascimento, ingresso, coordenador);
-                // docentesCadastrados.add(docente);
-                ppgi.insereDocente(docente);
-                linha = leituraDeArquivo.readLine();
-            }
+            FileReader arq1 = new FileReader("docentes.csv");
+            // FileReader arquivo = new FileReader("docentes.csv");
+            // BufferedReader leituraDeArquivo = new BufferedReader(arquivo);
+            BufferedReader fdocente = new BufferedReader(arq1);
+            ppgi.carregaArquivoDocentes(fdocente);
             ppgi.imprimeDocentes();
-            arquivo.close();
+            arq1.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
-        // System.out.println();
     }
-
-    // Métodos Gerais da Main
-
-    /*Imprime as informações de cada docente cadastrado*/
-    // private static void imprimeDocentes(){
-    //     for (Docente d: docentesCadastrados){
-    //         d.imprimeDocente();
-    //     }
-    // }
 
 }
--- src/trabalho/Sistema.java
+++ src/trabalho/Sistema.java
@@ -1,6 +1,10 @@
 package trabalho;
 
+import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.io.*;
+import java.text.ParseException;
 
 
 public class Sistema{
@@ -10,7 +14,41 @@ public class Sistema{
     
 
 
+
+
     /* ************************* DOCENTES ************************** */
+
+    public void carregaArquivoDocentes(BufferedReader arquivo){
+        try{
+            String linha = arquivo.readLine();
+            linha = arquivo.readLine();
+            while(linha != null){
+                String[] campos = linha.split(";");
+                long codigo = Long.parseLong(campos[0]);
+                String nome = campos[1];
+                Date nascimento = new SimpleDateFormat("dd/MM/yyyy").parse(campos[2]);
+                Date ingresso = new SimpleDateFormat("dd/MM/yyyy").parse(campos[3]);
+                boolean coordenador = false;
+                // System.out.println(linha);
+                if (campos.length == 5) {
+                    if (campos[4] == "X") {
+                        coordenador = true;
+                    }
+                }
+                Docente docente = new Docente(codigo, nome, nascimento, ingresso, coordenador);
+                // docentesCadastrados.add(docente);
+                insereDocente(docente);
+                linha = arquivo.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro: " + e.getMessage());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
 
     public void imprimeDocentes(){
         for (Map.Entry<Long,Docente> pair : docentesCadastrados.entrySet()) {
